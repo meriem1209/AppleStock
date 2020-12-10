f18d1e17f898ce9e076a77bbb2a4ecebe08c002e
Vayango Koné <vayango@hotmail.fr>
2020-12-10 13:06:34 +0100

    Premier commit

---
+++ AppleStock/MySqlConnector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using MySqlConnector;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppleStock
+{
+    class MySqlConnector
+    {
+        var builder = new MySqlConnectionStringBuilder
+        {
+            Server = "YOUR-SERVER.mysql.database.azure.com",
+            Database = "YOUR-DATABASE",
+            UserID = "USER@YOUR-SERVER",
+            Password = "PASSWORD",
+            SslMode = MySqlSslMode.Required,
+        };
+
+    }
+}
